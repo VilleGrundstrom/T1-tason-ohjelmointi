9ec5e81de3273e2fbb4574c929977108462de06e
VilleGrundstrom <59997735+VilleGrundstrom@users.noreply.github.com>
2021-09-08 15:10:24 +0300

    Add files via upload

---
+++ BMI-Laskuri/BMI-Laskuri/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace BMI_Laskuri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.painoTB = new System.Windows.Forms.TextBox();
+            this.pituusTB = new System.Windows.Forms.TextBox();
+            this.laskeBT = new System.Windows.Forms.Button();
+            this.vastausLB = new System.Windows.Forms.Label();
+            this.PainoLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna paino:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 103);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anna pituus:";
+            // 
+            // painoTB
+            // 
+            this.painoTB.Location = new System.Drawing.Point(231, 56);
+            this.painoTB.Name = "painoTB";
+            this.painoTB.Size = new System.Drawing.Size(94, 32);
+            this.painoTB.TabIndex = 2;
+            // 
+            // pituusTB
+            // 
+            this.pituusTB.Location = new System.Drawing.Point(231, 100);
+            this.pituusTB.Name = "pituusTB";
+            this.pituusTB.Size = new System.Drawing.Size(94, 32);
+            this.pituusTB.TabIndex = 3;
+            // 
+            // laskeBT
+            // 
+            this.laskeBT.Location = new System.Drawing.Point(136, 149);
+            this.laskeBT.Name = "laskeBT";
+            this.laskeBT.Size = new System.Drawing.Size(189, 31);
+            this.laskeBT.TabIndex = 4;
+            this.laskeBT.Text = "LaskePainoindeksi";
+            this.laskeBT.UseVisualStyleBackColor = true;
+            this.laskeBT.Click += new System.EventHandler(this.laskeBT_Click);
+            // 
+            // vastausLB
+            // 
+            this.vastausLB.AutoSize = true;
+            this.vastausLB.Location = new System.Drawing.Point(101, 203);
+            this.vastausLB.Name = "vastausLB";
+            this.vastausLB.Size = new System.Drawing.Size(163, 23);
+            this.vastausLB.TabIndex = 5;
+            this.vastausLB.Text = "Painoindeksisi on:";
+            // 
+            // PainoLB
+            // 
+            this.PainoLB.AutoSize = true;
+            this.PainoLB.Location = new System.Drawing.Point(105, 236);
+            this.PainoLB.Name = "PainoLB";
+            this.PainoLB.Size = new System.Drawing.Size(56, 23);
+            this.PainoLB.TabIndex = 6;
+            this.PainoLB.Text = "paino";
+            this.PainoLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 690);
+            this.Controls.Add(this.PainoLB);
+            this.Controls.Add(this.vastausLB);
+            this.Controls.Add(this.laskeBT);
+            this.Controls.Add(this.pituusTB);
+            this.Controls.Add(this.painoTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "LaskePainoindeksi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox painoTB;
+        private System.Windows.Forms.TextBox pituusTB;
+        private System.Windows.Forms.Button laskeBT;
+        private System.Windows.Forms.Label vastausLB;
+        private System.Windows.Forms.Label PainoLB;
+    }
+}
+
---
+++ BMI-Laskuri/BMI-Laskuri/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Laskuri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void laskeBT_Click(object sender, EventArgs e)
+        {
+            double paino = 0, pituus = 0;
+            paino = Convert.ToDouble(painoTB.Text);
+            pituus = Convert.ToDouble(pituusTB.Text);
+            double bmi = Math.Round(paino / (pituus * pituus), 2);
+            if(bmi < 18.5)
+            {
+                vastausLB.Text = "Painoindeksisi on " + bmi;
+                vastausLB.ForeColor = Color.Aqua;
+                PainoLB.Text = "Alipaino";
+                PainoLB.ForeColor = Color.Aqua;
+                PainoLB.Visible = true;
+            }
+            else if(bmi < 25)
+            {
+                vastausLB.Text = "Painoindeksisi on " + bmi;
+                vastausLB.ForeColor = Color.Green;
+                PainoLB.Text = "Normaalipaino";
+                PainoLB.ForeColor = Color.Green;
+                PainoLB.Visible = true;
+            }
+            else if(bmi < 40)
+            {
+                vastausLB.Text = "Painoindeksisi on " + bmi;
+                vastausLB.ForeColor = Color.Gold;
+                PainoLB.Text = "Ylipaino";
+                PainoLB.ForeColor = Color.Gold;
+                PainoLB.Visible = true;
+            }
+            else
+            {
+                vastausLB.Text = "Painoindeksisi on " + bmi;
+                vastausLB.ForeColor = Color.Red;
+                PainoLB.Text = "Huomattava ylipaino";
+                PainoLB.ForeColor = Color.Red;
+                PainoLB.Visible = true;
+            }
+        }
+    }
+}
