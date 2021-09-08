c34da8f0a4f81092f8bed85ded7b3df82d422250
VilleGrundstrom <59997735+VilleGrundstrom@users.noreply.github.com>
2021-09-08 11:18:25 +0300

    Add files via upload

---
+++ 6harjoitus/6harjoitus/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace _6harjoitus
+{
+    partial class SalasanaForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SalasanaPanel = new System.Windows.Forms.Panel();
+            this.KayttajatunnusLB = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.kayttajaTB = new System.Windows.Forms.TextBox();
+            this.SalasanaTB = new System.Windows.Forms.TextBox();
+            this.TarkistaBT = new System.Windows.Forms.Button();
+            this.virheLB = new System.Windows.Forms.Label();
+            this.SalasanaOikeinPanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SalasanaPanel.SuspendLayout();
+            this.SalasanaOikeinPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SalasanaPanel
+            // 
+            this.SalasanaPanel.BackColor = System.Drawing.Color.MidnightBlue;
+            this.SalasanaPanel.Controls.Add(this.virheLB);
+            this.SalasanaPanel.Controls.Add(this.TarkistaBT);
+            this.SalasanaPanel.Controls.Add(this.SalasanaTB);
+            this.SalasanaPanel.Controls.Add(this.kayttajaTB);
+            this.SalasanaPanel.Controls.Add(this.label2);
+            this.SalasanaPanel.Controls.Add(this.KayttajatunnusLB);
+            this.SalasanaPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SalasanaPanel.Location = new System.Drawing.Point(0, 0);
+            this.SalasanaPanel.Name = "SalasanaPanel";
+            this.SalasanaPanel.Size = new System.Drawing.Size(603, 241);
+            this.SalasanaPanel.TabIndex = 0;
+            this.SalasanaPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.virheLB_Paint);
+            // 
+            // KayttajatunnusLB
+            // 
+            this.KayttajatunnusLB.AutoSize = true;
+            this.KayttajatunnusLB.Location = new System.Drawing.Point(92, 91);
+            this.KayttajatunnusLB.Name = "KayttajatunnusLB";
+            this.KayttajatunnusLB.Size = new System.Drawing.Size(193, 33);
+            this.KayttajatunnusLB.TabIndex = 0;
+            this.KayttajatunnusLB.Text = "Käyttäjätunnus:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Salasana:";
+            // 
+            // kayttajaTB
+            // 
+            this.kayttajaTB.Location = new System.Drawing.Point(302, 100);
+            this.kayttajaTB.Name = "kayttajaTB";
+            this.kayttajaTB.Size = new System.Drawing.Size(255, 41);
+            this.kayttajaTB.TabIndex = 2;
+            // 
+            // SalasanaTB
+            // 
+            this.SalasanaTB.Location = new System.Drawing.Point(302, 147);
+            this.SalasanaTB.Name = "SalasanaTB";
+            this.SalasanaTB.Size = new System.Drawing.Size(255, 41);
+            this.SalasanaTB.TabIndex = 3;
+            // 
+            // TarkistaBT
+            // 
+            this.TarkistaBT.ForeColor = System.Drawing.Color.Black;
+            this.TarkistaBT.Location = new System.Drawing.Point(29, 142);
+            this.TarkistaBT.Name = "TarkistaBT";
+            this.TarkistaBT.Size = new System.Drawing.Size(122, 30);
+            this.TarkistaBT.TabIndex = 4;
+            this.TarkistaBT.Text = "Tarkista";
+            this.TarkistaBT.UseVisualStyleBackColor = true;
+            this.TarkistaBT.Click += new System.EventHandler(this.TarkistaBT_Click);
+            // 
+            // virheLB
+            // 
+            this.virheLB.AutoSize = true;
+            this.virheLB.Location = new System.Drawing.Point(29, 175);
+            this.virheLB.Name = "virheLB";
+            this.virheLB.Size = new System.Drawing.Size(73, 33);
+            this.virheLB.TabIndex = 5;
+            this.virheLB.Text = "virhe";
+            this.virheLB.Visible = false;
+            // 
+            // SalasanaOikeinPanel
+            // 
+            this.SalasanaOikeinPanel.BackColor = System.Drawing.Color.Maroon;
+            this.SalasanaOikeinPanel.Controls.Add(this.label3);
+            this.SalasanaOikeinPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SalasanaOikeinPanel.Font = new System.Drawing.Font("Segoe Script", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.SalasanaOikeinPanel.ForeColor = System.Drawing.Color.Snow;
+            this.SalasanaOikeinPanel.Location = new System.Drawing.Point(0, 0);
+            this.SalasanaOikeinPanel.Name = "SalasanaOikeinPanel";
+            this.SalasanaOikeinPanel.Size = new System.Drawing.Size(603, 241);
+            this.SalasanaOikeinPanel.TabIndex = 1;
+            this.SalasanaOikeinPanel.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(551, 80);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tervetuloa sivulleni";
+            // 
+            // SalasanaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 241);
+            this.Controls.Add(this.SalasanaOikeinPanel);
+            this.Controls.Add(this.SalasanaPanel);
+            this.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Gold;
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "SalasanaForm";
+            this.Text = "Salasanan Tarkastus";
+            this.SalasanaPanel.ResumeLayout(false);
+            this.SalasanaPanel.PerformLayout();
+            this.SalasanaOikeinPanel.ResumeLayout(false);
+            this.SalasanaOikeinPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel SalasanaPanel;
+        private System.Windows.Forms.Label virheLB;
+        private System.Windows.Forms.Button TarkistaBT;
+        private System.Windows.Forms.TextBox SalasanaTB;
+        private System.Windows.Forms.TextBox kayttajaTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label KayttajatunnusLB;
+        private System.Windows.Forms.Panel SalasanaOikeinPanel;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 6harjoitus/6harjoitus/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6harjoitus
+{
+    public partial class SalasanaForm : Form
+    {
+        public SalasanaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void virheLB_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void TarkistaBT_Click(object sender, EventArgs e)
+        {
+            
+                if (kayttajaTB.Text == "Ville" && SalasanaTB.Text == "sepp0")
+            {
+                SalasanaPanel.Visible = false;
+                SalasanaOikeinPanel.Visible = true;
+            }
+                else
+            {
+                virheLB.Text = "Käyttäjätunnus tai salasana virheellinen";
+                virheLB.Visible = true;
+            }
+                    
+        }
+    }
+}
