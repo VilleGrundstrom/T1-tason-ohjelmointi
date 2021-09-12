a182915cdde85298aef22b66586b6f2001c3f6f2
VilleGrundstrom <59997735+VilleGrundstrom@users.noreply.github.com>
2021-09-13 00:45:58 +0300

    Add files via upload

---
+++ Muistio/Muistio/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Muistio
+{
+
+    public partial class notepadForm : Form
+    {
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
+        private FontDialog fontDialog;
+        public notepadForm()
+        {
+            InitializeComponent();
+        }
+        private void UusiTiedosto()
+        {
+            try
+            {
+                if(!string.IsNullOrEmpty(MuistioTB.Text))
+                {
+                    MessageBox.Show("sinun pitää tallentaa ensin!");
+                }
+                else
+                {
+                    MuistioTB.Text = string.Empty;
+                    Text = "Nimetön";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("virhe: " + ex);
+            }
+        }
+        private void TallennaTiedosto()
+        {
+            try
+            {
+                if(!string.IsNullOrEmpty(MuistioTB.Text))
+                {
+                    saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "Teksti tiedosto (*.txt) | *.txt";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        File.WriteAllText(saveFileDialog.FileName, MuistioTB.Text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+            }
+        }
+        private void AvaaTiedosto()
+        {
+            try
+            {
+                openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    MuistioTB.Text = File.ReadAllText(openFileDialog.FileName);
+                    Text = openFileDialog.FileName;
+
+                }
+            }
+
+             catch (Exception ex)
+            {
+                MessageBox.Show("Virhe avattaessa tiedostoa!" + ex);
+            }
+        }
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void notepadForm_Load(object sender, EventArgs e)
+        {
+            fontDialog = new FontDialog();
+        }
+
+        private void uusiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UusiTiedosto();
+        }
+
+        private void avaaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AvaaTiedosto();
+        }
+
+        private void tallennaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TallennaTiedosto();
+        }
+
+        private void lopetaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(MuistioTB.Text))
+                {
+                    TallennaTiedosto();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+            }
+        }
+
+        private void kirjasinToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(fontDialog.ShowDialog() == DialogResult.OK)
+                {
+                    MuistioTB.Font = fontDialog.Font;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+            }
+        }
+    }
+}
