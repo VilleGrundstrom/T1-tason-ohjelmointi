c80669e146186845e92326d48cfb6d25563b317b
VilleGrundstrom <59997735+VilleGrundstrom@users.noreply.github.com>
2021-09-12 14:10:40 +0300

    Add files via upload

---
+++ Ajastin/Ajastin/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Ajastin
+{
+    partial class AjastinForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.minuutitCB = new System.Windows.Forms.ComboBox();
+            this.sekunnitCB = new System.Windows.Forms.ComboBox();
+            this.aikaLB = new System.Windows.Forms.Label();
+            this.startBT = new System.Windows.Forms.Button();
+            this.StopBT = new System.Windows.Forms.Button();
+            this.ajastinTM = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Minuutit:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sekunnit:";
+            // 
+            // minuutitCB
+            // 
+            this.minuutitCB.FormattingEnabled = true;
+            this.minuutitCB.Location = new System.Drawing.Point(19, 49);
+            this.minuutitCB.Name = "minuutitCB";
+            this.minuutitCB.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.minuutitCB.Size = new System.Drawing.Size(87, 31);
+            this.minuutitCB.TabIndex = 2;
+            // 
+            // sekunnitCB
+            // 
+            this.sekunnitCB.FormattingEnabled = true;
+            this.sekunnitCB.Location = new System.Drawing.Point(121, 50);
+            this.sekunnitCB.Name = "sekunnitCB";
+            this.sekunnitCB.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.sekunnitCB.Size = new System.Drawing.Size(83, 31);
+            this.sekunnitCB.TabIndex = 3;
+            // 
+            // aikaLB
+            // 
+            this.aikaLB.AutoSize = true;
+            this.aikaLB.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.aikaLB.Location = new System.Drawing.Point(19, 84);
+            this.aikaLB.Name = "aikaLB";
+            this.aikaLB.Size = new System.Drawing.Size(177, 73);
+            this.aikaLB.TabIndex = 4;
+            this.aikaLB.Text = "00:00";
+            // 
+            // startBT
+            // 
+            this.startBT.Location = new System.Drawing.Point(19, 160);
+            this.startBT.Name = "startBT";
+            this.startBT.Size = new System.Drawing.Size(87, 33);
+            this.startBT.TabIndex = 5;
+            this.startBT.Text = "Start";
+            this.startBT.UseVisualStyleBackColor = true;
+            this.startBT.Click += new System.EventHandler(this.startBT_Click);
+            // 
+            // StopBT
+            // 
+            this.StopBT.Location = new System.Drawing.Point(121, 160);
+            this.StopBT.Name = "StopBT";
+            this.StopBT.Size = new System.Drawing.Size(87, 33);
+            this.StopBT.TabIndex = 6;
+            this.StopBT.Text = "Stop";
+            this.StopBT.UseVisualStyleBackColor = true;
+            this.StopBT.Click += new System.EventHandler(this.StopBT_Click);
+            // 
+            // ajastinTM
+            // 
+            this.ajastinTM.Interval = 1000;
+            this.ajastinTM.Tick += new System.EventHandler(this.ajastinTM_Tick);
+            // 
+            // AjastinForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 207);
+            this.Controls.Add(this.StopBT);
+            this.Controls.Add(this.startBT);
+            this.Controls.Add(this.aikaLB);
+            this.Controls.Add(this.sekunnitCB);
+            this.Controls.Add(this.minuutitCB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "AjastinForm";
+            this.Text = "Ajastin";
+            this.Load += new System.EventHandler(this.AjastinForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox minuutitCB;
+        private System.Windows.Forms.ComboBox sekunnitCB;
+        private System.Windows.Forms.Label aikaLB;
+        private System.Windows.Forms.Button startBT;
+        private System.Windows.Forms.Button StopBT;
+        private System.Windows.Forms.Timer ajastinTM;
+    }
+}
+
---
+++ Ajastin/Ajastin/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Ajastin
+{
+    public partial class AjastinForm : Form
+    {
+        private int kokonaisaika;
+        public AjastinForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AjastinForm_Load(object sender, EventArgs e)
+        {
+            StopBT.Enabled = false;
+            for(int i = 0; < 60; i++)
+            {
+                minuutitCB.Items.Add(i.ToString());
+                sekunnitCB.Items.Add(i.ToString());
+            }
+            minuutitCB.SelectedIndex = 30;
+            minuutitCB.SelectedIndex = 0;
+        }
+
+        private void startBT_Click(object sender, EventArgs e)
+        {
+            startBT.Enabled = false;
+            StopBT.Enabled = true;
+            int minuutit = int.Parse(minuutitCB.SelectedItem.ToString());
+            int sekunnit = int.Parse(sekunnitCB.SelectedItem.ToString());
+            kokonaisaika = (minuutit * 60) + sekunnit;
+            ajastinTM.Enabled = true;
+        }
+
+        private void StopBT_Click(object sender, EventArgs e)
+        {
+            startBT.Enabled = true;
+            StopBT.Enabled = false;
+            kokonaisaika = 0;
+            ajastinTM.Enabled = false;
+            aikaLB.Text = "00:00";
+        }
+
+        private void ajastinTM_Tick(object sender, EventArgs e)
+        {
+            if(kokonaisaika > 0)
+            {
+                kokonaisaika--;
+                int minuutit = kokonaisaika / 60;
+                int sekunnit = kokonaisaika - (minuutit * 60);
+                aikaLB.Text = minuutit.ToString() + ":" + sekunnit.ToString();
+            }
+            else
+            {
+                ajastinTM.Stop();
+                MessageBox.Show("Aikasi loppui!");
+            }
+        }
+    }
+}
