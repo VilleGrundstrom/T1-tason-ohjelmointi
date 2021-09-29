5fdcb120af52a52e13b1987478fd3bf4e1e01a86
VilleGrundstrom <59997735+VilleGrundstrom@users.noreply.github.com>
2021-09-29 20:21:18 +0300

    Add files via upload

---
+++ Opiskelijanhallintajarjestelma/Opiskelijanhallintajarjestelma/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace Opiskelijanhallintajarjestelma
+{
+    partial class opiskelijataulu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.idTB = new System.Windows.Forms.TextBox();
+            this.etunimiTB = new System.Windows.Forms.TextBox();
+            this.sukunimiTB = new System.Windows.Forms.TextBox();
+            this.opiskelijanroTB = new System.Windows.Forms.TextBox();
+            this.emailTB = new System.Windows.Forms.TextBox();
+            this.puhelinTB = new System.Windows.Forms.TextBox();
+            this.tyhjennaBT = new System.Windows.Forms.Button();
+            this.tallennaBT = new System.Windows.Forms.Button();
+            this.paivitaBT = new System.Windows.Forms.Button();
+            this.poistaBT = new System.Windows.Forms.Button();
+            this.TietotauluDG = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.TietotauluDG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Etunimi:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(75, 131);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sukunimi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(332, 42);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Puhelin";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(332, 86);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Email:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(332, 131);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(162, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Opiskelijanumero:";
+            // 
+            // idTB
+            // 
+            this.idTB.Location = new System.Drawing.Point(167, 39);
+            this.idTB.Name = "idTB";
+            this.idTB.Size = new System.Drawing.Size(140, 29);
+            this.idTB.TabIndex = 6;
+            // 
+            // etunimiTB
+            // 
+            this.etunimiTB.Location = new System.Drawing.Point(167, 81);
+            this.etunimiTB.Name = "etunimiTB";
+            this.etunimiTB.Size = new System.Drawing.Size(140, 29);
+            this.etunimiTB.TabIndex = 7;
+            // 
+            // sukunimiTB
+            // 
+            this.sukunimiTB.Location = new System.Drawing.Point(167, 128);
+            this.sukunimiTB.Name = "sukunimiTB";
+            this.sukunimiTB.Size = new System.Drawing.Size(140, 29);
+            this.sukunimiTB.TabIndex = 8;
+            // 
+            // opiskelijanroTB
+            // 
+            this.opiskelijanroTB.Location = new System.Drawing.Point(503, 126);
+            this.opiskelijanroTB.Name = "opiskelijanroTB";
+            this.opiskelijanroTB.Size = new System.Drawing.Size(140, 29);
+            this.opiskelijanroTB.TabIndex = 9;
+            // 
+            // emailTB
+            // 
+            this.emailTB.Location = new System.Drawing.Point(503, 81);
+            this.emailTB.Name = "emailTB";
+            this.emailTB.Size = new System.Drawing.Size(140, 29);
+            this.emailTB.TabIndex = 10;
+            // 
+            // puhelinTB
+            // 
+            this.puhelinTB.Location = new System.Drawing.Point(503, 37);
+            this.puhelinTB.Name = "puhelinTB";
+            this.puhelinTB.Size = new System.Drawing.Size(140, 29);
+            this.puhelinTB.TabIndex = 11;
+            // 
+            // tyhjennaBT
+            // 
+            this.tyhjennaBT.Location = new System.Drawing.Point(29, 174);
+            this.tyhjennaBT.Name = "tyhjennaBT";
+            this.tyhjennaBT.Size = new System.Drawing.Size(167, 46);
+            this.tyhjennaBT.TabIndex = 12;
+            this.tyhjennaBT.Text = "Tyhjenna";
+            this.tyhjennaBT.UseVisualStyleBackColor = true;
+            this.tyhjennaBT.Click += new System.EventHandler(this.tyhjennaBT_Click);
+            // 
+            // tallennaBT
+            // 
+            this.tallennaBT.Location = new System.Drawing.Point(217, 174);
+            this.tallennaBT.Name = "tallennaBT";
+            this.tallennaBT.Size = new System.Drawing.Size(167, 46);
+            this.tallennaBT.TabIndex = 13;
+            this.tallennaBT.Text = "Tallenna";
+            this.tallennaBT.UseVisualStyleBackColor = true;
+            this.tallennaBT.Click += new System.EventHandler(this.tallennaBT_Click);
+            // 
+            // paivitaBT
+            // 
+            this.paivitaBT.Location = new System.Drawing.Point(407, 174);
+            this.paivitaBT.Name = "paivitaBT";
+            this.paivitaBT.Size = new System.Drawing.Size(167, 46);
+            this.paivitaBT.TabIndex = 14;
+            this.paivitaBT.Text = "päivitä";
+            this.paivitaBT.UseVisualStyleBackColor = true;
+            this.paivitaBT.Click += new System.EventHandler(this.paivitaBT_Click);
+            // 
+            // poistaBT
+            // 
+            this.poistaBT.Location = new System.Drawing.Point(597, 174);
+            this.poistaBT.Name = "poistaBT";
+            this.poistaBT.Size = new System.Drawing.Size(167, 46);
+            this.poistaBT.TabIndex = 15;
+            this.poistaBT.Text = "poista";
+            this.poistaBT.UseVisualStyleBackColor = true;
+            this.poistaBT.Click += new System.EventHandler(this.poistaBT_Click);
+            // 
+            // TietotauluDG
+            // 
+            this.TietotauluDG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TietotauluDG.Location = new System.Drawing.Point(8, 226);
+            this.TietotauluDG.Name = "TietotauluDG";
+            this.TietotauluDG.Size = new System.Drawing.Size(805, 300);
+            this.TietotauluDG.TabIndex = 16;
+            this.TietotauluDG.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TietotauluDG_CellClick);
+            // 
+            // opiskelijataulu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 538);
+            this.Controls.Add(this.TietotauluDG);
+            this.Controls.Add(this.poistaBT);
+            this.Controls.Add(this.paivitaBT);
+            this.Controls.Add(this.tallennaBT);
+            this.Controls.Add(this.tyhjennaBT);
+            this.Controls.Add(this.puhelinTB);
+            this.Controls.Add(this.emailTB);
+            this.Controls.Add(this.opiskelijanroTB);
+            this.Controls.Add(this.sukunimiTB);
+            this.Controls.Add(this.etunimiTB);
+            this.Controls.Add(this.idTB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "opiskelijataulu";
+            this.Text = "opiskelijajuttu";
+            this.Load += new System.EventHandler(this.opiskelijataulu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TietotauluDG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox idTB;
+        private System.Windows.Forms.TextBox etunimiTB;
+        private System.Windows.Forms.TextBox sukunimiTB;
+        private System.Windows.Forms.TextBox opiskelijanroTB;
+        private System.Windows.Forms.TextBox emailTB;
+        private System.Windows.Forms.TextBox puhelinTB;
+        private System.Windows.Forms.Button tyhjennaBT;
+        private System.Windows.Forms.Button tallennaBT;
+        private System.Windows.Forms.Button paivitaBT;
+        private System.Windows.Forms.Button poistaBT;
+        private System.Windows.Forms.DataGridView TietotauluDG;
+    }
+}
+
---
+++ Opiskelijanhallintajarjestelma/Opiskelijanhallintajarjestelma/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opiskelijanhallintajarjestelma
+{
+    public partial class opiskelijataulu : Form
+    {
+        OPISKELIJA opiskelija = new OPISKELIJA();
+        public opiskelijataulu()
+        {
+            InitializeComponent();
+        }
+
+        private void opiskelijataulu_Load(object sender, EventArgs e)
+        {
+            TietotauluDG.DataSource = opiskelija.haeOpiskelijat();
+        }
+
+        private void tyhjennaBT_Click(object sender, EventArgs e)
+        {
+            idTB.Text = "";
+            etunimiTB.Text = "";
+            sukunimiTB.Text = "";
+            puhelinTB.Text = "";
+            emailTB.Text = "";
+            opiskelijanroTB.Text = "";
+        }
+
+        private void tallennaBT_Click(object sender, EventArgs e)
+        {
+            String enimi = etunimiTB.Text;
+            String snimi = sukunimiTB.Text;
+            String puhelin = puhelinTB.Text;
+            String email = emailTB.Text;
+            int oNro = Int32.Parse(opiskelijanroTB.Text);
+
+            if (enimi.Trim().Equals("") || snimi.Trim().Equals("") || puhelin.Trim().Equals("") || email.Trim().Equals("") || oNro.Equals(""))
+            {
+                MessageBox.Show("VIRHE - Vaaditut kentät - Etu- ja sukunimi, puhelin, sähköposti ja opiskelijanumero", "Tyhjä kenttä", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Boolean lisaaAsiakas = opiskelija.lisaaOpiskelija(enimi, snimi, email, oNro);
+                if (lisaaAsiakas)
+                {
+                    MessageBox.Show("Uusi opiskelija lisätty onnistuneesti", "Opiskelijan lisäys", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Uutta opiskelijaa ei pystytty lisäämään", "Opiskelijan lisäys", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            TietotauluDG.DataSource = opiskelija.haeOpiskelijat();
+        }
+
+        private void paivitaBT_Click(object sender, EventArgs e)
+        {
+            String enimi = etunimiTB.Text;
+            String snimi = sukunimiTB.Text;
+            String puhelin = puhelinTB.Text;
+            String email = emailTB.Text;
+            int oNro = Int32.Parse(opiskelijanroTB.Text);
+            int oid = Int32.Parse(idTB.Text);
+
+            if (oid.Equals("") || enimi.Trim().Equals("") || snimi.Trim().Equals("") || puhelin.Trim().Equals("") || email.Trim().Equals("") || oNro.Equals(""))
+            {
+                MessageBox.Show("VIRHE - Vaaditut kentät - ID, Etu- ja sukunimi, puhelin, sähköposti ja opiskelijanumero", "Tyhjä kenttä", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Boolean lisaaAsiakas = opiskelija.muokkaaOpiskelijaa(oid, enimi, snimi, puhelin, email, oNro);
+                if (lisaaAsiakas)
+                {
+                    MessageBox.Show("Opiskelija päivitetty onnistuneesti", "Opiskelijan päivitys", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Opiskelijaa ei pystytty päivittämään", "Opiskelijan päivitys", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            TietotauluDG.DataSource = opiskelija.haeOpiskelijat();
+        }
+
+        private void TietotauluDG_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            idTB.Text = TietotauluDG.CurrentRow.Cells[0].Value.ToString();
+            etunimiTB.Text = TietotauluDG.CurrentRow.Cells[1].Value.ToString();
+            sukunimiTB.Text = TietotauluDG.CurrentRow.Cells[2].Value.ToString();
+            puhelinTB.Text = TietotauluDG.CurrentRow.Cells[3].Value.ToString();
+            emailTB.Text = TietotauluDG.CurrentRow.Cells[4].Value.ToString();
+            opiskelijanroTB.Text = TietotauluDG.CurrentRow.Cells[5].Value.ToString();
+        }
+
+        private void poistaBT_Click(object sender, EventArgs e)
+        {
+            String ktunnus = idTB.Text;
+            if (opiskelija.poistaOpiskelija(ktunnus))
+            {
+                TietotauluDG.DataSource = opiskelija.haeOpiskelijat();
+                MessageBox.Show("Opiskelija poistettu onnistuneesti", "Opiskelijan poisto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Opiskelijaa ei pystytty poistamaan", "Opiskelijan poisto", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            tyhjennaBT.PerformClick();
+        }
+    }
+}
