456fd880dbad0eb73491883e155c4c1048e85d8a
VilleGrundstrom <59997735+VilleGrundstrom@users.noreply.github.com>
2021-09-29 14:19:36 +0300

    Add files via upload

---
+++ ViidesHarjoitus/ViidesHarjoitus/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace ViidesHarjoitus
+{
+    partial class LukujenJarjestys
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lukuTB = new System.Windows.Forms.TextBox();
+            this.vastausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna luku(-999 lopetus):";
+            // 
+            // lukuTB
+            // 
+            this.lukuTB.Location = new System.Drawing.Point(268, 45);
+            this.lukuTB.Name = "lukuTB";
+            this.lukuTB.Size = new System.Drawing.Size(153, 29);
+            this.lukuTB.TabIndex = 1;
+            this.lukuTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lukuTB_KeyPress);
+            // 
+            // vastausLB
+            // 
+            this.vastausLB.AutoSize = true;
+            this.vastausLB.Location = new System.Drawing.Point(81, 103);
+            this.vastausLB.Name = "vastausLB";
+            this.vastausLB.Size = new System.Drawing.Size(65, 21);
+            this.vastausLB.TabIndex = 2;
+            this.vastausLB.Text = "vastaus";
+            this.vastausLB.Visible = false;
+            // 
+            // LukujenJarjestys
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 154);
+            this.Controls.Add(this.vastausLB);
+            this.Controls.Add(this.lukuTB);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "LukujenJarjestys";
+            this.Text = "Lukujen järjestys";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lukuTB;
+        private System.Windows.Forms.Label vastausLB;
+    }
+}
+
---
+++ ViidesHarjoitus/ViidesHarjoitus/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ViidesHarjoitus
+{
+    public partial class LukujenJarjestys : Form
+    {
+        List<int> jono = new List<int>();
+        public LukujenJarjestys()
+        {
+            InitializeComponent();
+        }
+
+        private void lukuTB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                if(lukuTB.Text == "-999")
+                {
+                    vastausLB.Text = "";
+                    int[] taulukko = jono.ToArray();
+                    Array.Sort(taulukko);
+                    foreach(var jasen in taulukko)
+                    {
+                        vastausLB.Text += jasen + " ";
+                    }
+                    vastausLB.Visible = true;
+                    
+                }
+                else
+                {
+                    jono.Add(Int32.Parse(lukuTB.Text));
+                    lukuTB.Text = "";
+                }
+            }
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                TyhjaaLomake();
+            }
+        }
+        private void TyhjaaLomake()
+        {
+            lukuTB.Text = "";
+        }
+    }
+}
